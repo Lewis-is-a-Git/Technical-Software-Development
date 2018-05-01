0a5c82145b3e3d1d0948176e907fcc3e5e790b2c
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-05-01 14:12:21 +1000

    Update BullsAndCows.cpp

--- Assignments/3/BullsAndCows.cpp
+++ Assignments/3/BullsAndCows.cpp
@@ -7,10 +7,10 @@ BullsAndCows::BullsAndCows()
 {
     for (int i = 0; i < 9; i++)
     {
-        fSecretNumbers[i] = i + 1;
+        fSecretNumbers[i] = i + 1; // constructor initalises an array from 1 to 9
     }   
 }
-void BullsAndCows::start()
+void BullsAndCows::start() //scrambles the array
 {
     srand(time(NULL));
     int n = 9;
@@ -21,7 +21,7 @@ void BullsAndCows::start()
         swap(fSecretNumbers[n], fSecretNumbers[k]);
     }
 }
-void BullsAndCows::guess( std::string aNumberString )
+void BullsAndCows::guess( std::string aNumberString ) //string of guesses
 {
     fBulls = 0;
     fCows = 0;
@@ -30,7 +30,7 @@ void BullsAndCows::guess( std::string aNumberString )
         int lTest = aNumberString[i] - '0';
         if (fSecretNumbers[i] == lTest)
         {
-            fBulls = fBulls + 1;
+            fBulls = fBulls + 1; //check if any numbers are in the correct place
         }
         else
         {
@@ -38,7 +38,7 @@ void BullsAndCows::guess( std::string aNumberString )
             {
                 if (fSecretNumbers[j] == lTest)
                 {
-                    fCows = fCows + 1;
+                    fCows = fCows + 1; //check if any numbers are correct but in the wrong place
                 }
             }
         }   
