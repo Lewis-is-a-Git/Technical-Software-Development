e06460276bfb18ea06c3f094ce43eb2683fe1a31
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-05-15 16:34:10 +1000

    Update BullsAndCowsController.cpp

--- Assignments/4/BullsAndCowsController.cpp
+++ Assignments/4/BullsAndCowsController.cpp
@@ -3,59 +3,51 @@
 #include <iostream>
 using namespace std;
 
- void BullsAndCowsController::startNewGame()
- {
-
- }
- /**
- * This method implements the guessing phase. It has to request a guess
- * from the user interface and test it with the model. The result should
- * be displayed in the user interface.
- *
- * @return true if the number of correctly guessed bulls is 4.
- */
- bool BullsAndCowsController::guess()
- {
-     return fGameModel.getBulls() == 4;
- }
+void BullsAndCowsController::startNewGame()
+{
+	fGameView.welcome(); //welcome message
+	fGameModel.start(); //creates the random solution array
+	fGameView.startNewGame(); //new game message
+}
+/**
+* This method implements the guessing phase. It has to request a guess
+* from the user interface and test it with the model. The result should
+* be displayed in the user interface.
+*
+* @return true if the number of correctly guessed bulls is 4.
+*/
+bool BullsAndCowsController::guess()
+{
+	return fGameModel.getBulls() == 4;
+}
 
 //public:
- /**
- * Constructor: Instantiates an object of class BullsAndCowsController.
- * The constructor establishes the references to model
- * and view objects. This is done by using a member
- * initializer for each.
- */
- BullsAndCowsController::BullsAndCowsController( BullsAndCows& aGameModel, lBullsAndCowsView& aGameView ):
-    fGameModel(aGameModel), fGameView(aGameView)
- {
- }
- /**
- * This method implements the "game loop". It asks the connected view to
- * to emit a welcome message and then initializes the random number generator,
- * so that the game produces different sequences every time it is played.
- * Compare with solution for problem set 3.
- * Then the game loop starts which repeatedly starts a new game and asks
- * the user to make guesses until all bulls have been found.
- * The method run coordinates these activities.
- * Selecting “n” as response to the question “New game, Y/N?”
- * breaks the “game loop”. In this case, the application has to
- * gracefully terminate with a “Game over, Good bye.” message.
- */
- void BullsAndCowsController::run()
- {
-     do
-     {
-         fGameView.welcome();
-         fGameModel.start();
-         fGameView.startNewGame();
-         do
-         {
-            fGameModel.guess(fGameView.guess());
-            fGameView.showGuess(fGameModel.getBulls(), fGameModel.getCows());
-         }
-         while (!guess());
-     }
-     while (fGameView.continueGame());
-     fGameView.endGame();
- }
+/**
+* Constructor: Instantiates an object of class BullsAndCowsController.
+* The constructor establishes the references to model
+* and view objects. This is done by using a member
+* initializer for each.
+*/
+BullsAndCowsController::BullsAndCowsController(BullsAndCows& aGameModel, lBullsAndCowsView& aGameView) :
+	fGameModel(aGameModel), fGameView(aGameView)
+{
+}
+/**
+* This method implements the "game loop". 
+* Selecting “n” as response to the question “New game, Y/N?”
+* breaks the “game loop”. In this case, the application has to
+* gracefully terminate with a “Game over, Good bye.” message.
+*/
+void BullsAndCowsController::run()
+{
+	do
+	{
+		startNewGame(); //creates the array and prints the welcome messages
+		do
+		{
+			fGameModel.guess(fGameView.guess()); //calcualte the number of cows and bulls based on the players guess
+			fGameView.showGuess(fGameModel.getBulls(), fGameModel.getCows()); //prints the number of bulls and cows to the console
+		} while (!guess()); //if the player has not won yet, continue the game
+	} while (fGameView.continueGame()); //ask the player to start a new game
+	fGameView.endGame(); //game over message
+}
