276a725ca49e099bb5b990e9ecbfa886b19afba7
Lewis-is-a-Git <36223578+Lewis-is-a-Git@users.noreply.github.com>
2018-05-15 16:33:37 +1000

    Update BullsAndCowsConsoleView.cpp

--- Assignments/4/BullsAndCowsConsoleView.cpp
+++ Assignments/4/BullsAndCowsConsoleView.cpp
@@ -3,37 +3,37 @@
 using namespace std;
 string BullsAndCowsConsoleView::guess()
 {
-    string playerGuess;
-    cout << "Make a guess: ";
-    cin >> playerGuess;
-    cout << endl;
-    return playerGuess;
+	string playerGuess;
+	cout << "Make a guess: ";
+	cin >> playerGuess;
+	cout << endl;
+	return playerGuess;
 }
-void BullsAndCowsConsoleView::showGuess( int aBulls, int aCows )
+void BullsAndCowsConsoleView::showGuess(int aBulls, int aCows)
 {
-    cout << "Number of Bulls: " << aBulls << ", number of Cows: " << aCows << endl;
+	cout << "Number of Bulls: " << aBulls << ", number of Cows: " << aCows << endl;
 }
 void BullsAndCowsConsoleView::welcome()
 {
-    cout << "Bulls and Cows, brought to you by Lewis Brockman-Horlsey (101533222)" << endl;
+	cout << "Bulls and Cows, brought to you by Lewis Brockman-Horlsey (101533222)" << endl;
 }
 void BullsAndCowsConsoleView::startNewGame()
 {
-    cout << "New Game" << endl;
+	cout << "New Game" << endl;
 }
 bool BullsAndCowsConsoleView::continueGame()
 {
-    char lQuery;
-    cout << "New Game? [Y/N]:";
-    cin >> lQuery;
-    cout << endl;
-    if (lQuery == 'y' || lQuery == 'Y')
-    {
-        return true;
-    }
-    return false;
+	char lQuery;
+	cout << "New Game? [Y/N]:";
+	cin >> lQuery;
+	cout << endl;
+	if (lQuery == 'y' || lQuery == 'Y')
+	{
+		return true;
+	}
+	return false;
 }
 void BullsAndCowsConsoleView::endGame()
 {
-    cout << "Game Over. Good bye";
+	cout << "Game Over. Good bye";
 }
