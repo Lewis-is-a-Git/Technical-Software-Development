49c25d3ef4c32a0f61070fc5e3336669d385e9bb
Lewis Brockman-Horsley <radiation@Live.com.au>
2018-05-25 16:09:56 +1000

    added test 1

---
+++ Tests/1/DataEncoder.cpp
@@ -0,0 +1,74 @@
+/*
+	Data Encoder
+	Works with the data decoder made in assignment 1
+	@author Lewis Brockman-Horsley
+	Date created: 13/4/2018
+*/
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void encodeTextFile(ifstream& aInfile, ofstream& aOutfile)
+{
+    char lCharacterBuffer[4]; //buffer to store the 4 characters in reverse order before encoding
+    char lCharacter;	//temp character holdcer from file
+    int lDataToRead = 4; //counter
+
+    aInfile.get(lCharacter);
+    while (aInfile.good())
+    {
+        if (lCharacter != 13) //if not a newline character
+        {
+            lDataToRead--; //need to decrement because the chars are stored in reverse order
+            lCharacterBuffer[lDataToRead] = lCharacter; //stored in reverse order
+            if (lDataToRead == 0) // when the buffer array is full
+            {
+                int lInteger32 = 0; //create an integer to encode the characters
+                for (int k = 0; k < 4; k++)
+                {
+                    lInteger32 = lInteger32 * 256 + lCharacterBuffer[k]; //add the ascii value of the char
+                }
+                aOutfile << lInteger32 << '\n'; //save it to the file
+                lDataToRead = 4; //reset the buffer counter
+            }
+        }
+        aInfile.get(lCharacter);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+	//requires 2 command line arguments
+	//arg1 = input file, arg2 output.txt
+	if (argc != 3)
+	{
+		cerr << "Incorrect use of command line arguments:" << endl;
+		cerr << "Encoder inputfile.txt outputfile.txt" << endl;
+		return 1;
+	}
+	
+	ifstream lInput;		//create the file streams
+	ofstream lOutput;
+	lInput.open(argv[1], ios::in);
+	lOutput.open(argv[2], ios::out);
+    //File validations
+	if (!lInput.good())
+	{
+		cerr << "Cannot open file: " << argv[2] << endl;
+		lOutput.close();
+		return 2;
+	}
+	if (!lOutput.good())
+	{
+		cerr << "Cannot create file: " << argv[3] << endl;
+		lInput.close();
+		return 3;
+	}
+
+    encodeTextFile(lInput, lOutput);
+
+	lOutput.close();
+	lInput.close();
+	return 0;
+}
